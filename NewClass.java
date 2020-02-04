4557a61bf821a876ef59f5f106317d3699282149
Andres Castillo <60480414+anthonymao931@users.noreply.github.com>
2020-02-04 18:44:51 -0500

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,10 @@
+
+public class NewClass {
+    public static void main(String[] args){
+        int contador =0;
+        for(int i=1; i<10; i+=2){
+            contador=contador+i;
+        }
+        System.out.println(contador);
+    }
+}
