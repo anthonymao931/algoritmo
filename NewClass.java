c4ca022a06550b69fb1ae3865b72c735f6ad2a8a
Andres Castillo <60480414+anthonymao931@users.noreply.github.com>
2020-02-04 18:47:49 -0500

    Update NewClass.java

--- NewClass.java
+++ NewClass.java
@@ -2,7 +2,7 @@
 public class NewClass {
     public static void main(String[] args){
         int contador =0;
-        for(int i=1; i<10; i+=2){
+        for(int i=1; i<100000000; i+=2){
             contador=contador+i;
         }
         System.out.println(contador);
